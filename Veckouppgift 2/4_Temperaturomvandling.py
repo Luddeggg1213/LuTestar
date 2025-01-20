9bbae01120bba8242490a00b5384caf5255afae6
Ludde <ludvig.guste@gmail.com>
2025-01-20 16:58:58 +0100

    Veckouppgift 4 tempovandling
    Signed-off-by: Ludde <ludvig.guste@gmail.com>

--- Veckouppgift 2/4_Temperaturomvandling.py
+++ Veckouppgift 2/4_Temperaturomvandling.py
@@ -6,10 +6,10 @@ print (float(celsius) * 1.8 + 32)
 #4 2 Har inte fått att fungera, får syntax fel men förstår inte alls varför
 temp = str((input("Välj Celsius ange C eller Farhenheit ange F:")))
 
-if temp == C:
-    C = input ("Ange Celsius")
+if temp == "C":
+    C = input ("Ange Celsius ")
     print ((float(C) * 1.8 + 32))
 
-elif temp == F:
-    C = input("Ange Farhenheit")
+elif temp == "F":
+    F = input("Ange Farhenheit ")
     print ((float(F) - 32)/ 1.8)
